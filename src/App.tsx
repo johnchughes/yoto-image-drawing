e26b030aa505ccd0a20c2af34cece52fda10d9d7
John Hughes <j.c.hughes7@gmail.com>
2024-06-05 23:56:47 +0100

    added null pixel style and export

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,11 @@ function App() {
 
   const PIXEL_SIZE: number = 25;
   const GRID_SIZE: number = 16;
+  const DEFAULT_COLOUR: string | null = null;
 
   const [colours, setColours] = useState<string[]>([]);
   const colourPicker = useRef<HTMLInputElement>(null);
-  const [pixels, setPixels] = useState<string[][]>(new Array(GRID_SIZE).fill("#000000").map(() => new Array(GRID_SIZE).fill("#000000")));
+  const [pixels, setPixels] = useState<string[][]>(new Array(GRID_SIZE).fill(DEFAULT_COLOUR).map(() => new Array(GRID_SIZE).fill(DEFAULT_COLOUR)));
 
   const exportCanvas = useRef<HTMLCanvasElement>(null);
 
@@ -57,9 +58,18 @@ function App() {
 
     for (let x = 0; x < pixels.length; x++) {
       for (let y = 0; y < pixels[x].length; y++) {
-        ctx.rect(x, y, 1, 1);
-        ctx.fillStyle = pixels[x][y];
-        ctx.fillRect(x, y, 1, 1);
+        const pixel = pixels[x][y];
+        console.log(pixel);
+        if (pixel) {
+          ctx.rect(x, y, 1, 1);
+          ctx.fillStyle = pixels[x][y];
+          ctx.fillRect(x, y, 1, 1);
+        }
+        else {
+          ctx.globalAlpha = 0;
+          ctx.fillRect(x, y, 1, 1);
+          ctx.globalAlpha = 1;
+        }
       }
     }
 
@@ -69,6 +79,11 @@ function App() {
     aDownloadLink.href = image;
     aDownloadLink.click();
 
+  }
+
+  const fill = () => {
+    const colourHex: string = colourPicker.current?.value as string;
+    setPixels(new Array(GRID_SIZE).fill(colourHex).map(() => new Array(GRID_SIZE).fill(colourHex)))
   }
 
   return (
@@ -85,8 +100,11 @@ function App() {
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <input ref={colourPicker} type="color" />
 
+          <button onClick={fill}>fill</button>
+
+
           <div style={{ display: 'flex', flexDirection: 'column' }}>
-            {colours.map(colour => <div key={colour} onClick={() => { onColourClicked(colour) }} style={{ width: PIXEL_SIZE, height: PIXEL_SIZE, marginRight: 8, background: colour }}></div>)}
+            {colours.map(colour => <div key={colour} onClick={() => { onColourClicked(colour) }} style={{ width: PIXEL_SIZE, height: PIXEL_SIZE, marginTop: 8, background: colour, border: "1px solid black", }}></div>)}
           </div>
 
         </div>
@@ -101,7 +119,20 @@ function App() {
           return (
             <div style={{ display: "flex", flexDirection: "row" }} key={`row_${pxi}`}>
               {px.map((py, pyi) => {
-                return <div key={`pixel_${pxi}_${pyi}`} onClick={() => onPixelClicked(pxi, pyi)} style={{ width: PIXEL_SIZE, height: PIXEL_SIZE, marginRight: '8px', marginBottom: '8px', border: "1px solid black", background: py }}></div>
+
+                const chequered = {
+                  'backgroundImage': "linear-gradient(45deg, #bbbdbf 25%, transparent 25%),linear-gradient(45deg, transparent 75%, #bbbdbf 75%),linear-gradient(45deg, transparent 75%, #bbbdbf 75%),linear-gradient(45deg, #bbbdbf 25%, #fff 25%)",
+                  'backgroundSize': `${PIXEL_SIZE}px ${PIXEL_SIZE}px`,
+                  'backgroundPosition': `0 0, 0 0, -${PIXEL_SIZE / 2}px -${PIXEL_SIZE / 2}px, ${PIXEL_SIZE / 2}px ${PIXEL_SIZE / 2}px`
+                };
+
+                const backgroundColour = {
+                  'backgroundColor': py
+                }
+
+                const pixel_background = py ? backgroundColour : chequered;
+
+                return <div key={`pixel_${pxi}_${pyi}`} onClick={() => onPixelClicked(pxi, pyi)} style={{ width: PIXEL_SIZE, height: PIXEL_SIZE, marginRight: '8px', marginBottom: '8px', border: "1px solid black", ...pixel_background }}></div>
               })}
             </div>
           )
