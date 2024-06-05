26ca4239463d4fb2d30b6334dfff3102000c59a7
John Hughes <j.c.hughes7@gmail.com>
2024-06-05 23:34:21 +0100

    added basic export

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,22 @@ import { useRef, useState } from "react"
 
 function App() {
 
+  const PIXEL_SIZE: number = 25;
   const GRID_SIZE: number = 16;
 
   const [colours, setColours] = useState<string[]>([]);
   const colourPicker = useRef<HTMLInputElement>(null);
   const [pixels, setPixels] = useState<string[][]>(new Array(GRID_SIZE).fill("#000000").map(() => new Array(GRID_SIZE).fill("#000000")));
-  
 
-  const onPixelClicked = (x:number, y:number) => {
+  const exportCanvas = useRef<HTMLCanvasElement>(null);
 
-    const selectedColour : string = colourPicker.current?.value as string;
+  const onPixelClicked = (x: number, y: number) => {
+
+    const selectedColour: string = colourPicker.current?.value as string;
 
     const nextPixels = pixels.map((_x, _xi) => {
       return _x.map((_y, _yi) => {
-        if(_xi === x && _yi === y) {
+        if (_xi === x && _yi === y) {
           return selectedColour;
         }
         return _y;
@@ -29,44 +31,92 @@ function App() {
 
     setPixels(nextPixels);
 
-    if(colours.indexOf(selectedColour) == -1) {
+    if (colours.indexOf(selectedColour) == -1) {
       setColours((current) => [...current, selectedColour]);
     }
 
   }
 
-  const onColourClicked = (colourHex:string) => {
+  const onColourClicked = (colourHex: string) => {
     //set colour picker value. 
+    if (colourPicker.current) {
+      colourPicker.current.value = colourHex;
+    }
+  }
+
+  const exportPNG = () => {
+    if (!exportCanvas.current) {
+      alert("canvas being fucky, can't export.");
+      return;
+    }
+
+    var ctx = exportCanvas.current.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+
+    for (let x = 0; x < pixels.length; x++) {
+      for (let y = 0; y < pixels[x].length; y++) {
+        ctx.rect(x, y, 1, 1);
+        ctx.fillStyle = pixels[x][y];
+        ctx.fillRect(x, y, 1, 1);
+      }
+    }
+
+
+    // Convert the canvas to data
+    var image = exportCanvas.current.toDataURL();
+    // Create a link
+    var aDownloadLink = document.createElement('a');
+    // Add the name of the file to the link
+    aDownloadLink.download = 'my_yoto_image.png';
+    // Attach the data to the link
+    aDownloadLink.href = image;
+    // Get the code to click the download link
+    aDownloadLink.click();
+
   }
 
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "row", margin: 8 }}>
 
-      <div style={{display:'flex', flexDirection:'column'}}>
-        <input ref={colourPicker} type="color" />
+      <div style={{ display: 'fex', flexDirection: 'column', border: "1px solid black" }}>
 
-        <div style={{display:'flex', flexDirection: 'row', height:50}}>
-          {colours.map(colour => <div onClick={() => {onColourClicked(colour)}} style={{width:50, height:50,marginRight:8, background: colour}}></div> )}
+        <div style={{ display: 'fex', flexDirection: 'column' }}>
+          <canvas ref={exportCanvas} width={16} height={16}></canvas>
+          <button style={{ display: 'block' }} onClick={() => exportPNG()}>export</button>
+        </div>
+
+
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <input ref={colourPicker} type="color" />
+
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            {colours.map(colour => <div key={colour} onClick={() => { onColourClicked(colour) }} style={{ width: PIXEL_SIZE, height: PIXEL_SIZE, marginRight: 8, background: colour }}></div>)}
+          </div>
+
         </div>
 
       </div>
 
 
-
-      <div style={{display: 'flex', flexDirection:'column', margin:16}}>
+      <div style={{ display: 'flex', flexDirection: 'column', margin: 16 }}>
 
         {pixels.map((px, pxi) => {
 
           return (
-            <div style={{display:"flex", flexDirection: "row"}} key={`row_${pxi}`}>
+            <div style={{ display: "flex", flexDirection: "row" }} key={`row_${pxi}`}>
               {px.map((py, pyi) => {
-                return <div key={`pixel_${pxi}_${pyi}`} onClick={() => onPixelClicked(pxi, pyi)} style={{width:'50px', height:'50px', marginRight: '8px', marginBottom:'8px', border:"1px solid black",  background: py}}></div>
+                return <div key={`pixel_${pxi}_${pyi}`} onClick={() => onPixelClicked(pxi, pyi)} style={{ width: PIXEL_SIZE, height: PIXEL_SIZE, marginRight: '8px', marginBottom: '8px', border: "1px solid black", background: py }}></div>
               })}
             </div>
           )
         })}
 
       </div>
+
+
+
     </div>
   )
 }
